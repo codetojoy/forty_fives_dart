21aa123b3e23a0210fb2203084009a259b6ef2d4
Michael Easter <codetojoy@gmail.com>
2021-06-26 15:31:03 -0300

    incremental update Sat 26 Jun 2021 15:31:03 ADT

--- test/ranker_test.dart
+++ test/ranker_test.dart
@@ -19,6 +19,51 @@ const tsTRUMP_PLAYED = TrickState.TRUMP_PLAYED;
 
 void main() {
   group('Ranker', () {
+    test('customSort diamonds', () {
+      var trump = Suit.DIAMONDS;
+      var cards = [
+        Card(Ordinal.KING, trump),
+        Card(Ordinal.QUEEN, trump),
+        Card(Ordinal.JACK, trump),
+        Card(Ordinal.TEN, trump),
+        Card(Ordinal.NINE, trump),
+        Card(Ordinal.EIGHT, trump),
+        Card(Ordinal.SEVEN, trump),
+        Card(Ordinal.SIX, trump),
+        Card(Ordinal.FIVE, trump),
+        Card(Ordinal.FOUR, trump),
+        Card(Ordinal.THREE, trump),
+        Card(Ordinal.TWO, trump),
+        Card(Ordinal.ACE, trump),
+        Card(Ordinal.ACE, Suit.HEARTS),
+      ];
+
+      cards.shuffle();
+
+      // test
+      Ranker(trump, Suit.UNKNOWN).customSortArray(cards);
+
+      var i = 0;
+      expect(cards[i++].ordinal, Ordinal.TWO);
+      expect(cards[i++].ordinal, Ordinal.THREE);
+      expect(cards[i++].ordinal, Ordinal.FOUR);
+      expect(cards[i++].ordinal, Ordinal.SIX);
+      expect(cards[i++].ordinal, Ordinal.SEVEN);
+      expect(cards[i++].ordinal, Ordinal.EIGHT);
+      expect(cards[i++].ordinal, Ordinal.NINE);
+      expect(cards[i++].ordinal, Ordinal.TEN);
+      expect(cards[i++].ordinal, Ordinal.QUEEN);
+      expect(cards[i++].ordinal, Ordinal.KING);
+
+      expect(cards[i].ordinal, Ordinal.ACE);
+      expect(cards[i++].suit, Suit.DIAMONDS);
+
+      expect(cards[i].ordinal, Ordinal.ACE);
+      expect(cards[i++].suit, Suit.HEARTS);
+
+      expect(cards[i++].ordinal, Ordinal.JACK);
+      expect(cards[i++].ordinal, Ordinal.FIVE);
+    });
     test('customSort hearts', () {
       var trump = Suit.HEARTS;
       var cards = [
