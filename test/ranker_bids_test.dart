6d83c763e5ca73fa8fe788d91fa4448938118d2b
Michael Easter <codetojoy@gmail.com>
2021-06-27 09:20:18 -0300

    more Ranker tests

---
+++ test/ranker_bids_test.dart
@@ -0,0 +1,12 @@
+import 'package:test/test.dart';
+
+import 'package:forty_fives_dart/models/card.dart';
+import 'package:forty_fives_dart/services/ranker.dart';
+
+void main() {
+  group('Ranker', () {
+    test('bids', () {
+      // test
+    });
+  }); // group
+} // main
