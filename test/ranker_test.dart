f316625c83562af3e0b039a1e3568ab2e73c6ece
Michael Easter <codetojoy@gmail.com>
2021-06-27 08:48:46 -0300

    incremental update Sun 27 Jun 2021 08:48:46 ADT

--- test/ranker_test.dart
+++ test/ranker_test.dart
@@ -25,14 +25,7 @@ void main() {
       return cards;
     }
 
-    test('customSort diamonds', () {
-      const trump = Suit.DIAMONDS;
-      final cards = _buildCards(trump, true);
-      cards.shuffle();
-
-      // test
-      Ranker(trump, Suit.UNKNOWN).customSortArray(cards);
-
+    void assertRedTrump(List<Card> cards, Suit suit) {
       var i = 0;
       expect(cards[i++].ordinal, Ordinal.TWO);
       expect(cards[i++].ordinal, Ordinal.THREE);
@@ -45,14 +38,50 @@ void main() {
       expect(cards[i++].ordinal, Ordinal.QUEEN);
       expect(cards[i++].ordinal, Ordinal.KING);
 
-      expect(cards[i].ordinal, Ordinal.ACE);
-      expect(cards[i++].suit, Suit.DIAMONDS);
+      if (suit != Suit.HEARTS) {
+        expect(cards[i].ordinal, Ordinal.ACE);
+        expect(cards[i++].suit, suit);
+      }
 
       expect(cards[i].ordinal, Ordinal.ACE);
       expect(cards[i++].suit, Suit.HEARTS);
 
       expect(cards[i++].ordinal, Ordinal.JACK);
       expect(cards[i++].ordinal, Ordinal.FIVE);
+    }
+
+    void assertBlackTrump(List<Card> cards, Suit suit) {
+      var i = 0;
+      expect(cards[i++].ordinal, Ordinal.TEN);
+      expect(cards[i++].ordinal, Ordinal.NINE);
+      expect(cards[i++].ordinal, Ordinal.EIGHT);
+      expect(cards[i++].ordinal, Ordinal.SEVEN);
+      expect(cards[i++].ordinal, Ordinal.SIX);
+      expect(cards[i++].ordinal, Ordinal.FOUR);
+      expect(cards[i++].ordinal, Ordinal.THREE);
+      expect(cards[i++].ordinal, Ordinal.TWO);
+      expect(cards[i++].ordinal, Ordinal.QUEEN);
+      expect(cards[i++].ordinal, Ordinal.KING);
+
+      expect(cards[i].ordinal, Ordinal.ACE);
+      expect(cards[i++].suit, suit);
+
+      expect(cards[i].ordinal, Ordinal.ACE);
+      expect(cards[i++].suit, Suit.HEARTS);
+
+      expect(cards[i++].ordinal, Ordinal.JACK);
+      expect(cards[i++].ordinal, Ordinal.FIVE);
+    }
+
+    test('customSort diamonds', () {
+      const trump = Suit.DIAMONDS;
+      final cards = _buildCards(trump, true);
+      cards.shuffle();
+
+      // test
+      Ranker(trump, Suit.UNKNOWN).customSortArray(cards);
+
+      assertRedTrump(cards, trump);
     });
     test('customSort hearts', () {
       const trump = Suit.HEARTS;
@@ -62,20 +91,7 @@ void main() {
       // test
       Ranker(trump, Suit.UNKNOWN).customSortArray(cards);
 
-      var i = 0;
-      expect(cards[i++].ordinal, Ordinal.TWO);
-      expect(cards[i++].ordinal, Ordinal.THREE);
-      expect(cards[i++].ordinal, Ordinal.FOUR);
-      expect(cards[i++].ordinal, Ordinal.SIX);
-      expect(cards[i++].ordinal, Ordinal.SEVEN);
-      expect(cards[i++].ordinal, Ordinal.EIGHT);
-      expect(cards[i++].ordinal, Ordinal.NINE);
-      expect(cards[i++].ordinal, Ordinal.TEN);
-      expect(cards[i++].ordinal, Ordinal.QUEEN);
-      expect(cards[i++].ordinal, Ordinal.KING);
-      expect(cards[i++].ordinal, Ordinal.ACE);
-      expect(cards[i++].ordinal, Ordinal.JACK);
-      expect(cards[i++].ordinal, Ordinal.FIVE);
+      assertRedTrump(cards, trump);
     });
     test('customSort clubs', () {
       const trump = Suit.CLUBS;
@@ -85,26 +101,7 @@ void main() {
       // test
       Ranker(trump, Suit.UNKNOWN).customSortArray(cards);
 
-      var i = 0;
-      expect(cards[i++].ordinal, Ordinal.TEN);
-      expect(cards[i++].ordinal, Ordinal.NINE);
-      expect(cards[i++].ordinal, Ordinal.EIGHT);
-      expect(cards[i++].ordinal, Ordinal.SEVEN);
-      expect(cards[i++].ordinal, Ordinal.SIX);
-      expect(cards[i++].ordinal, Ordinal.FOUR);
-      expect(cards[i++].ordinal, Ordinal.THREE);
-      expect(cards[i++].ordinal, Ordinal.TWO);
-      expect(cards[i++].ordinal, Ordinal.QUEEN);
-      expect(cards[i++].ordinal, Ordinal.KING);
-
-      expect(cards[i].ordinal, Ordinal.ACE);
-      expect(cards[i++].suit, Suit.CLUBS);
-
-      expect(cards[i].ordinal, Ordinal.ACE);
-      expect(cards[i++].suit, Suit.HEARTS);
-
-      expect(cards[i++].ordinal, Ordinal.JACK);
-      expect(cards[i++].ordinal, Ordinal.FIVE);
+      assertBlackTrump(cards, trump);
     });
     test('customSort spades', () {
       const trump = Suit.SPADES;
@@ -114,6 +111,9 @@ void main() {
       // test
       Ranker(trump, Suit.UNKNOWN).customSortArray(cards);
 
+      assertBlackTrump(cards, trump);
+    });
+    void assertBlackNonTrump(List<Card> cards) {
       var i = 0;
       expect(cards[i++].ordinal, Ordinal.TEN);
       expect(cards[i++].ordinal, Ordinal.NINE);
@@ -125,26 +125,21 @@ void main() {
       expect(cards[i++].ordinal, Ordinal.TWO);
       expect(cards[i++].ordinal, Ordinal.QUEEN);
       expect(cards[i++].ordinal, Ordinal.KING);
-
-      expect(cards[i].ordinal, Ordinal.ACE);
-      expect(cards[i++].suit, Suit.SPADES);
-
-      expect(cards[i].ordinal, Ordinal.ACE);
-      expect(cards[i++].suit, Suit.HEARTS);
-
+      expect(cards[i++].ordinal, Ordinal.ACE);
       expect(cards[i++].ordinal, Ordinal.JACK);
       expect(cards[i++].ordinal, Ordinal.FIVE);
-    });
-    assertBlackNonTrump(List<Card> cards) {
+    }
+
+    void assertRedNonTrump(List<Card> cards) {
       var i = 0;
-      expect(cards[i++].ordinal, Ordinal.TEN);
-      expect(cards[i++].ordinal, Ordinal.NINE);
-      expect(cards[i++].ordinal, Ordinal.EIGHT);
-      expect(cards[i++].ordinal, Ordinal.SEVEN);
-      expect(cards[i++].ordinal, Ordinal.SIX);
-      expect(cards[i++].ordinal, Ordinal.FOUR);
-      expect(cards[i++].ordinal, Ordinal.THREE);
       expect(cards[i++].ordinal, Ordinal.TWO);
+      expect(cards[i++].ordinal, Ordinal.THREE);
+      expect(cards[i++].ordinal, Ordinal.FOUR);
+      expect(cards[i++].ordinal, Ordinal.SIX);
+      expect(cards[i++].ordinal, Ordinal.SEVEN);
+      expect(cards[i++].ordinal, Ordinal.EIGHT);
+      expect(cards[i++].ordinal, Ordinal.NINE);
+      expect(cards[i++].ordinal, Ordinal.TEN);
       expect(cards[i++].ordinal, Ordinal.QUEEN);
       expect(cards[i++].ordinal, Ordinal.KING);
       expect(cards[i++].ordinal, Ordinal.ACE);
@@ -171,6 +166,26 @@ void main() {
       Ranker(suit, Suit.UNKNOWN).customSortArray(cards);
 
       assertBlackNonTrump(cards);
+    });
+    test('customSort diamonds non-trump', () {
+      const suit = Suit.DIAMONDS;
+      final cards = _buildCards(suit, false);
+      cards.shuffle();
+
+      // test
+      Ranker(suit, Suit.UNKNOWN).customSortArray(cards);
+
+      assertRedNonTrump(cards);
+    });
+    test('customSort hearts non-trump', () {
+      const suit = Suit.HEARTS;
+      final cards = _buildCards(suit, false);
+      cards.shuffle();
+
+      // test
+      Ranker(suit, Suit.UNKNOWN).customSortArray(cards);
+
+      assertRedNonTrump(cards);
     });
   }); // group
 } // main
